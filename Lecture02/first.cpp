ed469e36ac5bea8e1b18c9721f49d2bff89c8c18
Prashant Kumar verma <pk0085853@gmail.com>
2026-03-10 03:04:13 +0530

    first commit

---
+++ Lecture02/first.cpp
@@ -0,0 +1,104 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+
+    // int a, b;
+    // cout << "Enter your first number:";
+    // cin >> a;
+    // cout << "Enter your second number:";
+    // cin >> b;
+
+    // cout << "Your sum is: " << a + b;
+
+    // int c = 'a' + 1;
+    // cout << c;
+
+    // Arithmetic operator :
+    // + - *  / % 
+    // int a = 20;
+    // int b = 10;
+
+    // cout << a + b << endl;
+    // cout << a - b << endl;
+    // cout << a * b << endl;
+    // cout << a / b << endl;
+    // cout << a % b << endl;
+       
+    // Assignment operator:
+
+    // a += b;
+    // a = a + b;
+
+    // a -= b;
+    // a = a - b; 
+
+    // a *= b;
+    // a = a * b; 
+
+    // a /= b;
+    // a = a / b; 
+
+    // cout << a;
+
+    // Comparison operator:
+    // >, <, >=, <=, !=, 
+    // int a = 20;
+    // int b = 10;
+
+    // cout << (a>b);
+    // cout << (a<b);
+    // cout << (a>=b);
+    // cout << (a<=b);
+    // cout << (a!=b);
+
+    // int a = 20;
+
+    // Left shift 
+    // a = a << 3;
+
+    // Right shift
+    // a = a >> 3;
+
+    // cout << a;
+
+    // Logical operator:
+    // && (and) : Both should be true --> Then only true, else false.
+    // || (or) : If both side is false --> then only false, else true.
+    //  ! (Not operator) : Reverse.
+
+    // cout << (true && true);
+    // cout << (true && false);
+    // cout << (true || true);
+    // cout << (true || false);
+    // cout << (false || false);
+    // cout << (!true);
+    // cout << (!false);
+
+    // Increment/ Decrement operator:
+
+    // int a = 10;
+    
+    // pre increment , post increment
+    // pre decrement , post decrement
+
+    // int b = a++;
+    //int b = ++a;
+    // int b = a--;
+    // int b = --a;
+
+    // cout << b << endl;
+    // cout << a << endl;
+
+    // Bitwise operator:
+    // & , | , ^
+
+    int a = 10;
+    int b = 5;
+
+    cout << (a & b) << endl;
+    cout << (a | b) << endl;
+    cout << (a ^ b) << endl;
+
+    return 0;
+}
---
+++ Lecture02/second.cpp
@@ -0,0 +1,129 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    // int age;
+
+    // cout << "Enter your age: ";
+    // cin >> age;
+
+    // if(age >= 18) {
+    //     cout << "You are eligible for vote.";
+    // } 
+    // else {
+    //     cout << "You are not eligible for vote.";
+    // }
+
+    // int marks;
+
+    // cout << "Enter your marks: ";
+    // cin >> marks;
+
+    // if(marks >= 33) {
+    //     cout << "You are pass.";
+    // }
+    // else {
+    //     cout << "You are fail.";
+    // }
+
+    // if(marks > 90) {
+    //     cout << "Grade: A+";
+    // }
+    // else if(marks > 80 && marks <= 90) {
+    //     cout << "Grade: A";
+    // }
+    // else if(marks > 70 && marks <= 80) {
+    //     cout << "Grade: B+";
+    // }
+    // else if(marks > 60 && marks <= 70) {
+    //     cout << "Grade: B";
+    // }
+    // else {
+    //     cout << "Grade: C";
+    // }
+
+    // int day;
+    // cout << "Enter your day: ";
+    // cin >> day;
+
+    // switch (day)
+    // {
+    // case 1: {
+    //         cout << "Monday";
+    //     break;
+    // }
+    // case 2: {
+    //         cout << "Tuesday";
+    //     break;
+    // }
+    // case 3: {
+    //         cout << "Wednesday";
+    //     break;
+    // }
+    // case 4: {
+    //         cout << "Thursday";
+    //     break;
+    // }
+    // case 5: {
+    //         cout << "Friday";
+    //     break;
+    // }
+    // case 6: {
+    //         cout << "Saturday";
+    //     break;
+    // }
+    // case 7: {
+    //         cout << "Sunday";
+    //     break;
+    // }
+    
+    // default:{
+    //     cout << "Invalid day!";
+    //     break;
+    // }
+    // }
+
+    // Even or Odd :
+    
+    // int num;
+    // cout << "Enter your Number: ";
+    // cin >> num;
+
+    // if(num % 2 == 0) {
+    //     cout << "Even Number";
+    // }
+    // else{
+    //     cout << "Odd Number";
+    // }
+
+    // int num1 = 15;
+    // int num2 = 14;
+
+    // if(num1 > num2) {
+    //     cout << "num1 is greater.";
+    // }
+    // else if(num1 < num2) {
+    //     cout << "num2 is greater.";
+    // }
+    // else {
+    //     cout << "Both are equal.";
+    // }
+
+    // Loops:
+
+    // cout << "Hello Coder Army\n";
+    // cout << "Hello Coder Army\n";
+    // cout << "Hello Coder Army\n";
+    // cout << "Hello Coder Army\n";
+
+    // for(int i = 1; i <= 10; i++) {
+    //     cout << "Hello Coder Army\n";
+    // }
+
+    for(int i = 1; i <= 10; i++) {
+        cout << i << " ";
+    }
+
+    return 0;
+}
