9beec48a46cb5487310bb07db1d0edb6fc3665d0
Prashant Kumar verma <pk0085853@gmail.com>
2026-03-14 17:46:57 +0530

    Add initial implementation of first.cpp with various commented code examples

---
+++ Lecture03/first.cpp
@@ -0,0 +1,273 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    // Even Number:
+    // for (int i = 2; i <= 20; i = i+2) {
+    //     cout<< i << endl;
+    // };
+
+    // for(int i = 2; i <= 20; i++) {
+    //     if(i % 2 == 0) {
+    //         cout<<i<<endl;
+    //     }
+    // };
+
+    //Odd Number:
+    // for(int i = 1; i <= 20; i = i+2) {
+    //     cout<<i<<endl;
+    // };
+    
+    // print a to z:
+    // for (char alpha = 'a'; alpha <= 'z'; alpha++) {
+    //     cout<<alpha<<endl;
+    // };
+    
+    // Sum of first 10 Numbers:
+    // int sum = 0;
+    // for(int i = 1; i <= 10; i++) {
+    //     sum = sum + i; // Or sum+=i;
+    // }
+    // cout<<sum<<endl;
+
+    // Sum of square first 10 Natural Numbers:
+    // int sum = 0;
+    // for(int i = 1; i <= ; i++) {
+    //     sum = sum + i*i;
+    // };
+    // cout<<sum<<endl;
+
+    // 1 2 3 4 5
+    // 1 2 3 4 5
+    // 1 2 3 4 5
+    // 1 2 3 4 5
+    // 1 2 3 4 5
+    // 1 2 3 4 5
+
+    // for(int j = 1; j <= 6; j++) {
+
+    //     for(int i = 1; i <= 5; i++) {
+    //         cout << i << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // a b c d e
+    // a b c d e
+    // a b c d e
+    // a b c d e
+    // a b c d e
+
+    // for(char j = 1; j <= 5; j++) {
+
+    //     for(char i = 'a'; i <= 'e'; i++) {
+    //         cout << i << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // for(int i = 1; i <= 5; i++) {
+
+    //     cout << "Hello Coder Army";
+    //     cout << endl;
+    // }
+
+    // 10 to 1 Number:
+    // for(int i = 10; i >= 1; i--) {
+    //     cout << i << " ";
+    // }
+
+    // Table Program :
+    // int table = 7;
+    // for(int i = 1; i <= 10; i++) {
+    //     cout << table*i << endl;
+    // }
+
+    // *****
+    // *****
+    // *****
+    // *****
+    // *****
+    
+    // for(int j = 0; j < 5; j++) {
+
+    //     for(int i = 0; i < 5; i++) {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+    // *
+    // **
+    // ***
+    // ****
+    // *****
+
+    // for(int i = 0; i < 1; i++) {
+    //     cout << "*";
+    // }
+    // cout << endl;
+
+    // for(int i = 0; i < 2; i++) {
+    //     cout << "*";
+    // }
+    // cout << endl;
+
+    // for(int i = 0; i < 3; i++) {
+    //     cout << "*";
+    // }
+    // cout << endl;
+
+    // for(int i = 0; i < 4; i++) {
+    //     cout << "*";
+    // }
+    // cout << endl;
+
+    // for(int i = 0; i < 5; i++) {
+    //     cout << "*";
+    // }
+    // cout << endl;
+
+    // for(int j = 1; j <= 5; j++) {
+
+    //     for(int i = 0; i < j; i++) {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+    // *****
+    // ****
+    // ***
+    // **
+    // *
+
+    // for(int j = 5; j >= 1; j--) {
+
+    //     for (int i = 0; i < j; i++)
+    //     {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+    //     *
+    //    **
+    //   ***
+    //  ****
+    // *****
+
+    // for(int j = 0; j < 4; j++) {
+    //         cout << " ";
+    // }    
+
+    // for(int j = 0; j < 1; j++) {
+    //         cout << "*";
+    // }
+
+    // cout << endl;
+
+    // for(int j = 0; j < 3; j++) {
+    //         cout << " ";
+    // }    
+
+    // for(int j = 0; j < 2; j++) {
+    //         cout << "*";
+    // }
+    
+    // cout << endl;
+
+    // for(int j = 0; j < 2; j++) {
+    //         cout << " ";
+    // }    
+
+    // for(int j = 0; j < 3; j++) {
+    //         cout << "*";
+    // }
+    
+    // cout << endl;
+
+    // for(int j = 0; j < 1; j++) {
+    //         cout << " ";
+    // }    
+
+    // for(int j = 0; j < 4; j++) {
+    //         cout << "*";
+    // }
+    
+    // cout << endl;
+
+    // for(int j = 0; j < 0; j++) {
+    //         cout << " ";
+    // }    
+
+    // for(int j = 0; j < 5; j++) {
+    //         cout << "*";
+    // }
+    
+    // cout << endl;
+
+    // for(int i = 4; i >= 0; i--) {
+
+    //     for(int j = 0; j < i; j++) {
+    //            cout << " ";
+    //    }    
+   
+    //    for(int j = 0; j < 5 - i; j++) {
+    //            cout << "*";
+    //    }
+   
+    //    cout << endl;
+    // }
+
+    // 1 2 3 4 5
+    // 5 7 8 9 10
+    // 11 12 13 14 15
+    // 16 17 18 19 20
+    // 21 22 23 24 25
+    
+    // for(int i = 1; i <= 5; i++) {
+    //     cout << i << " ";
+    // }
+    // cout << endl;
+
+    // for(int i = 6; i <= 10; i++) {
+    //     cout << i << " ";
+    // }
+    // cout << endl;
+
+    // for(int i = 11; i <= 15; i++) {
+    //     cout << i << " ";
+    // }
+    // cout << endl;
+    // for(int i = 16; i <= 20; i++) {
+    //     cout << i << " ";
+    // }
+    // cout << endl;
+    // for(int i = 21; i <= 25; i++) {
+    //     cout << i << " ";
+    // }
+    // cout << endl;
+
+    // for(int j = 1; j <=21; j = j+5) {
+        
+    //     for(int i = j; i <= j+4; i++) {
+    //     cout << i << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    int num = 1;
+
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 5; j++) {
+            cout << num << " ";
+            num++;
+        }
+        cout << endl;
+    }
+
+
+    return 0;
+}
